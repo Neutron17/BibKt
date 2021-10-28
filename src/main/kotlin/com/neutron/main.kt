956c51c02c09e7ca03ef6759956bab7369ac6da5
Neutron17 <sandormatyas17@gmail.com>
2021-10-28 23:19:02 +0200

    1.1

--- src/main/kotlin/com/neutron/main.kt
+++ src/main/kotlin/com/neutron/main.kt
@@ -1,45 +1,31 @@
-import com.neutron.*
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.Serializable
+import com.neutron.BookName
+import com.neutron.Chapter
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.apache.commons.cli.*
-import org.apache.commons.csv.CSVFormat
-import org.apache.commons.csv.CSVRecord
 import java.io.File
 import java.io.FileNotFoundException
-import java.io.FileReader
-import java.math.BigInteger
-import java.net.URL
 import java.util.*
 import kotlin.properties.Delegates
 import kotlin.system.exitProcess
 
-
-/*
-{"reference":"Genesis 1:1","verses":
-[{"book_id":"GEN","book_name":"Genesis","chapter":1,"verse":1,"text":"In principio creavit Deus cælum et terram."}],
-"text":"In principio creavit Deus cælum et terram.",
-"translation_id":"clementine","translation_name":"Clementine Latin Vulgate","translation_note":"Public Domain"}
-* */
-
-
-
 val metadata: MutableList<Pair<String, Int>> = mutableListOf()
 
-@Serializable
-data class Third(val value: MutableList<String>)
-
-@Serializable
-data class Inner(val value: Third)
-
-@Serializable
-data class Outer(@Contextual val inner: Inner)
+enum class Translation { KJV, Vulgate, Karoli }
+fun Translation.toStr() = when(this) {
+    Translation.KJV ->
+        "kjv"
+    Translation.Vulgate ->
+        "vulgate"
+    Translation.Karoli ->
+        "karoli"
+}
 
 lateinit var book: String
+lateinit var translation: Translation
 var chapt by Delegates.notNull<Int>()
 var vers by Delegates.notNull<Int>()
+var isDebug: Boolean = false
 
 class Main { companion object {
         @JvmStatic
@@ -52,7 +38,7 @@ class Main { companion object {
                     metadata.add(Pair(it.name, i))
                 }
             }
-            println(Json.decodeFromString<Chapter>(read("bibles/karoli/$book/$chapt.json")).verses[vers+1].text)
+            println(Json.decodeFromString<Chapter>(read("bibles/${translation.toStr()}/$book/$chapt.json")).verses[vers-1].text)
             /*val reader = FileReader("new.csv")
             val json = Json { prettyPrint = true }
             val records:Iterable<CSVRecord> = CSVFormat.DEFAULT.withHeader().parse(reader)
@@ -73,9 +59,16 @@ fun parse(args: Array<String>) {
     run {
         val verse: String
         val options = Options()
-        val verseopt = Option("v", "verse", true, "")
-        verseopt.isRequired = true
-        options.addOption(verseopt)
+        val verseOpt = Option("v", "verse", true, "-v <Book> <Chapter(number)> <Verse(number)>")
+        val transOpt = Option("t", "translation", true,
+            "kjv / en (King James Version)\nkaroli / hu (Károli - Hungarian)\nvulgate / vul / lat (Vulgate - Latin)")
+        val debugOpt = Option("d", "debug", false, "Set debug flag on")
+        verseOpt.isRequired = true
+        debugOpt.isRequired = false
+        transOpt.isRequired = false
+        options.addOption(verseOpt)
+        options.addOption(transOpt)
+        options.addOption(debugOpt)
         val parser: CommandLineParser = DefaultParser()
         val formatter = HelpFormatter()
         val cmd: CommandLine //not a good practice, it serves it purpose
@@ -87,12 +80,35 @@ fun parse(args: Array<String>) {
             exitProcess(1)
         }
         verse = cmd.getOptionValue("verse").replace(",", " ").replace(":", " ")
+        if(cmd.hasOption("debug")) isDebug = true
         val foo = verse.split(" ")
-        if(foo.size < 3)
+        if(foo.size < 3) {
+            if(isDebug)
+                println("debug: argument count: ${foo.size}")
             error("Not enough arguments")
+        }
         book = foo[0]
         chapt = foo[1].toInt()
         vers = foo[2].toInt()
+        if(isDebug)
+            println("debug: $book $chapt $vers")
+        val value = cmd.getOptionValue("translation")
+        translation = when(value) {
+            "en", "kjv" ->
+                Translation.KJV
+            "lat", "val", "vulgate" ->
+                Translation.Vulgate
+            "karoli", "hu" ->
+                Translation.Karoli
+            null ->
+                Translation.KJV
+            else -> {
+                System.err.println("Invalid translation setting")
+                exitProcess(1)
+            }
+        }
+        if(isDebug)
+            println("debug: ${translation.toStr()} $value")
     }
 }
 
